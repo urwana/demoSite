038f57180253c1be2fc05ef41e3572db5d0e1ef6
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-03-18 22:47:15 +0900

    画像変更

--- parts/top/card.php
+++ parts/top/card.php
@@ -4,10 +4,18 @@
   <div class="card__image">
     <div class="imgWrapper-card">
       <picture>
-        <source media="(min-width: 768px)" srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>.jpg" width="335" height="170">
-        <source media="(max-width: 767px)" srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>.jpg" width="335" height="170">
-        <source media="(max-width: 767px) and (-webkit-min-device-pixel-ratio: 3), (max-width: 767px) and (min-resolution: 384dpi)" srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>-sp@3x.jpg" width="335" height="170">
-        <img src="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>.jpg" alt="サービスイメージ" width="335" height="170">
+        <source media="(min-width: 768px)"
+          srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>@2x.jpg" width="335" height="170">
+        <source media="(min-width: 768px) and (-webkit-min-device-pixel-ratio: 2)"
+          srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>@2x.jpg" width="335" height="170">
+        <source media="(max-width: 767px)"
+          srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>.jpg" width="335" height="170">
+        <source
+          media="(max-width: 767px) and (-webkit-min-device-pixel-ratio: 3), (max-width: 767px) and (min-resolution: 384dpi)"
+          srcset="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>-sp@3x.jpg" width="335"
+          height="170">
+        <img src="<?php echo esc_url(get_theme_file_uri($element['serviceImg'])); ?>.jpg" alt="サービスイメージ" width="335"
+          height="170">
       </picture>
       <div class="pictureText">
         <div class="serviceNumber"><?php echo $element['serviceNumber']; ?></div>
